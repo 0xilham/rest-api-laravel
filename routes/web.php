28c41a397859e457902e49db4eab873d2f1163b4
0xilham <ilhamhermawan@hotmail.com>
2025-05-11 00:44:38 +0700

    feat: setup get api posts controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('api')->group(function () {
+Route::get('/posts', [PostController::class, 'index']);
 });
